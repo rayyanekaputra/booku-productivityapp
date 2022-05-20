ee2adc86f1771247b396c0cdaacb0aeec8911e18
sabansk <sabdha.kun@gmail.com>
2022-05-20 10:50:05 +0800

    onPress function test on notes.js for back-end mechanism

--- components/Notes.js
+++ components/Notes.js
@@ -19,11 +19,86 @@ export default function Notes(){
         return null;
     }
     return(
+<<<<<<< Updated upstream
         <View style = {styles.container}>
             <Text style = {styles.text}>
                 notess mauko mencatat
             </Text>
         </View>
+=======
+         <ScrollView style={styles.container} contentInsetAdjustmentBehavior = 'automatic' showsVerticalScrollIndicator = {false}>
+             {/* header */}
+            <View style = {styles.headerWrapper}>
+                <Text style = {styles.textUtama}>Notes</Text>
+                <TouchableOpacity onPress ={()=>{
+          navigation.navigate('Editor')
+        }}>
+                <AntDesign name="plus" size={24} color= {colors.antiHitam} />
+                </TouchableOpacity>
+            </View>
+
+            {/*pengingat takut lupa  */}
+            <TouchableOpacity style = {styles.focusCall} onPress ={()=>{
+          navigation.navigate('DatesTab', {screen: 'Dates'})
+        }}>
+                {/* barisan pertama */}
+                <View style = {{
+                    paddingHorizontal: 24,
+                    paddingTop: 24,
+                    paddingBottom: 8,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}>
+                    <Text style = {styles.textUtamaDalam} >
+                        Takut Lupa?
+                    </Text>
+                    <AntDesign name="caretright" size={16} color= {colors.antiHitam} />
+                </View>
+                {/* barisan kedua */}
+                <View style = {{
+                    paddingHorizontal: 24,
+                    paddingBottom: 24,
+                }}>
+                    <Text style = {styles.subtextUtamaDalam} >
+                    Jangan lupa set reminder tugas-tugas kamu agar semua tetap on-track!
+                    </Text>
+                    
+                </View>
+                
+            </TouchableOpacity>
+            {/* timer pomodoro */}
+            <View style = {[styles.timerPomodoro, {
+                marginBottom: 16,
+            }]}>
+                {/* barisan pertama */}
+                <View style = {{
+                    paddingHorizontal: 24,
+                    paddingTop: 24,
+                }}>
+                    <Text style = {styles.textUtamaDalam} >
+                        Mulai Belajar
+                    </Text>
+
+                </View>
+                {/* barisan kedua */}
+                <View style = {{
+                    paddingHorizontal: 24,
+                    paddingBottom: 24,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center',
+                }}>
+                    <Text style = {styles.subtextUtamaDalam} >
+                        Dengan metode pomodoro
+                    </Text>
+                    <TouchableOpacity style = {styles.buttonInside} onPress={() => 
+                      }>
+                        <Text style = {styles.textButtonInside}>Reset Timer</Text>
+                    </TouchableOpacity>
+                                
+                            
+>>>>>>> Stashed changes
 
     );
 
